f4ecfaa22375ced0ed58c6868e60d88aff113645
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-11-01 14:00:22 +0900

    Add deploy script

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import env, cd, run
+from fabric.contrib.files import exists
+from settings import HOST, REMOTE_HOME, REMOTE_PYTHON_HOME, KEY_PATH
+
+env.hosts = [HOST]
+env.key_filename = KEY_PATH
+
+def deploy():
+    with cd(REMOTE_HOME):
+        if exists("current"):
+            run("rm -rf current")
+            run("mkdir current")
+        with cd("repo"):
+            run("git remote update")
+            run("git archive web | tar -x -f - -C %s/current" % REMOTE_HOME)
+        with cd("current"):
+            run("%s/bin/python setup.py install --force" % REMOTE_PYTHON_HOME)
+        run('touch .uwsgi_touch')
