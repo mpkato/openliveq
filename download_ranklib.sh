b6e95b9edf557767ead7dc6e59c4add8d14a13bb
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-11-12 16:51:21 +0900

    Delete a download script for RankLib

--- download_ranklib.sh
+++
@@ -1,5 +0,0 @@
-CURDIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-
-URL=https://sourceforge.net/projects/lemur/files/lemur/RankLib-2.1/RankLib-2.1-patched.jar/download
-FILENAME=RankLib.jar
-wget $URL -O $CURDIR/$FILENAME
