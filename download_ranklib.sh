b097b7490734c7be1d3c33fa16102051298fe006
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-11-12 16:39:11 +0900

    Remove ranklib.py

--- download_ranklib.sh
+++ download_ranklib.sh
@@ -1,5 +1,5 @@
 CURDIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 URL=https://sourceforge.net/projects/lemur/files/lemur/RankLib-2.1/RankLib-2.1-patched.jar/download
-FILENAME=RankLib-2.1.jar
-wget $URL -O $CURDIR/openliveq/$FILENAME
+FILENAME=RankLib.jar
+wget $URL -O $CURDIR/$FILENAME
--- openliveq/__init__.py
+++ openliveq/__init__.py
@@ -5,5 +5,4 @@ from .clickthrough import Clickthrough
 from .instance import Instance
 from .collection import Collection
 from .feature_factory import FeatureFactory
-from .ranklib import Ranklib
 from .click_model import ClickModel
