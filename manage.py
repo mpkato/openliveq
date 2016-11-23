2e9553b3b1eafe5d0681619b9f13343cdd894541
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-11-23 14:26:59 +0900

    Fix a bug in unload

--- manage.py
+++ manage.py
@@ -12,12 +12,11 @@ def unload():
     from web.query import Query
     import openliveq as olq
     from openliveq.db import SessionContextFactory
-    if exists(DBPATH):
-        scf = SessionContextFactory()
-        engine = scf.session_factory.engine
-        olq.Question.__table__.drop(engine)
-        olq.Clickthrough.__table__.drop(engine)
-        Query.__table__.drop(engine)
+    scf = SessionContextFactory()
+    engine = scf.session_factory.engine
+    olq.Question.__table__.drop(engine)
+    olq.Clickthrough.__table__.drop(engine)
+    Query.__table__.drop(engine)
 
 if __name__ == "__main__":
     manager.run()
