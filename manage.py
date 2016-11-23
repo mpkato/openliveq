547e3b1eb9bab92390160d6ebcafc1b4a6c9cc18
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-11-23 14:32:26 +0900

    Try bug fix

--- manage.py
+++ manage.py
@@ -14,9 +14,10 @@ def unload():
     from openliveq.db import SessionContextFactory
     scf = SessionContextFactory()
     engine = scf.session_factory.engine
-    olq.Question.__table__.drop(engine)
-    olq.Clickthrough.__table__.drop(engine)
-    Query.__table__.drop(engine)
+    olq.Question.__table__.drop(engine, checkfirst=True)
+    olq.Clickthrough.__table__.drop(engine, checkfirst=True)
+    Query.__table__.drop(engine, checkfirst=True)
+    print("DONE!")
 
 if __name__ == "__main__":
     manager.run()
